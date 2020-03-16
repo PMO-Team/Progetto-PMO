7fc56a28235352eef79ed04505a491538edb2787
Nicholas Kania <nicholas.nick.kania@gmail.com>
2020-03-16 02:10:13 +0100

    Added some testing stuff
    The code is referring at BTCParser before it was been deleted and rewritten (only public interfaces and the encoding methods)

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BTC;
 
 namespace Testing
 {
@@ -6,7 +7,50 @@ namespace Testing
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*BTCObject mObj = new BTCObject();
+            mObj.Add("player", new BTCString("simo"));
+            mObj.Add("pg-name", new BTCString("Kirox"));
+            mObj.Add("pg-level", new BTCNumber(3));
+            BTCObject wp = new BTCObject();
+            wp.Add("weapon-type", new BTCString("bow"));
+            wp.Add("weapon-range", new BTCNumber(64));
+            wp.Add("weapon-destroyed", new BTCBool(false));
+            mObj.Add("weapon", wp);
+            BTCList items = new BTCList();
+            IBTCData item = new BTCObject();
+            ((BTCObject) item).Add("name", new BTCString("Kirinite"));
+            ((BTCObject) item).Add("description", new BTCString("A fantasy mineral"));
+            items.Add(item);
+            items.Add(new BTCString("Health Potion (12)"));
+            mObj.Add("inventory", items);
+            items = new BTCList();
+            items.Add(new BTCNumber(1));
+            items.Add(new BTCNumber(2));
+            items.Add(new BTCNumber(3));
+            items.Add(new BTCNumber(4));
+            //items.Add()
+            ((BTCList) mObj.Tag("inventory")).Add(items);
+
+            Console.WriteLine(BTCParser.Encode(mObj, true));
+            Console.WriteLine("\n\n" + BTCParser.Encode(mObj));
+            
+            BTCParser.EncodeIntoFile(mObj,
+                System.AppDomain.CurrentDomain.BaseDirectory + "../../../../generated.btc", true);*/
+
+            try
+            {
+                BTCObject myObject = BTCParser.ParseFromFile(System.AppDomain.CurrentDomain.BaseDirectory + 
+                                    "../../../../generated.btc");
+
+                myObject.Add("new-entry", new BTCString("Sono appena stato aggiunto"));
+
+                BTCParser.EncodeIntoFile(myObject,
+                    System.AppDomain.CurrentDomain.BaseDirectory + "../../../../generated.btc", true);
+            }
+            catch (BTCSyntaxErrorException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
