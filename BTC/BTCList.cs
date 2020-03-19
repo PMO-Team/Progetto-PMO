77fc6b7f720eaf30f174fc46844a8b371028390f
Nicholas Kania <nicholas.nick.kania@gmail.com>
2020-03-20 00:09:03 +0100

    Modified BTCList
    Modified BTCList class rappresentation

--- BTC/BTCList.cs
+++ BTC/BTCList.cs
@@ -2,36 +2,57 @@ using System.Collections.Generic;
 
 namespace BTC
 {
-	public class BTCList : List<IBTCData>, IBTCData
+	public class BTCList : IBTCData
 	{
+		private List<IBTCData> elements;
+
+		public BTCList()
+		{
+			this.elements = new List<IBTCData>();
+		}
+
 		public void Add(BTCString i)
 		{
-			this.Add(i);
+			this.elements.Add(i);
 		}
 		public void Add(BTCNumber i)
 		{
-			this.Add(i);
+			this.elements.Add(i);
 		}
 		public void Add(BTCBool i)
 		{
-			this.Add(i);
+			this.elements.Add(i);
 		}
 		public void Add(BTCObject i)
 		{
-			this.Add(i);
+			this.elements.Add(i);
 		}
 		public void Add(BTCList i)
 		{
-			this.Add(i);
+			this.elements.Add(i);
 		}
+		
+		public void RemoveAt(int index)
+		{
+			this.elements.RemoveAt(index);
+		}
+		public int Count()
+		{
+			return this.elements.Count;
+		}
+		public IBTCData At(int index)
+		{
+			return this.elements[index];
+		}
+
 		public string Encode()
 		{
 			string result = "[";
 
-			for (int i = 0; i < this.Count; i++)
+			for (int i = 0; i < this.elements.Count; i++)
 			{
-				result += this[i].Encode();
-				if (i != (this.Count - 1))
+				result += this.elements[i].Encode();
+				if (i != (this.elements.Count - 1))
 					result += "|"; 
 			}
 
@@ -46,11 +67,11 @@ namespace BTC
 			for (var i = 0; i < (separators - 1); i++)
 				sep += "\t";
 
-			for (int i = 0; i < this.Count; i++)
+			for (int i = 0; i < this.elements.Count; i++)
 			{
 				result += sep + "\t";
-				result += this[i].Encode(separators++);
-				if (i != (this.Count - 1))
+				result += this.elements[i].Encode(separators++);
+				if (i != (this.elements.Count - 1))
 					result += " |";
 				result += "\r\n";
 			}
